cb36deeedece7b9df5f982f9d973f8c59bdc2464
AndrewG <andrew@andrewg.xyz>
2023-01-03 22:50:50 -0500

    Moved some module placements

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -4,9 +4,9 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	// {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	// {"",	"sb-tasks",	10,	26},
-	{"",	"sb-music",	0,	11},
-	{"",	"sb-packages",	0,	8},
-	{"",	"sb-news",		0,	6},
+	{"",	"sb-music",	5,	11},
+	{"",	"sb-packages",	38000,	8},
+	{"",	"sb-news",		18000,	6},
 	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
 	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
@@ -17,9 +17,9 @@ static const Block blocks[] = {
 	/* {"",	"sb-cpu",		10,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-volume",	0,	10},
+	{"",	"sb-internet",	5,	4},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
